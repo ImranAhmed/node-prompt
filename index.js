3b067516f8f0423a155fd7cfde15aa984e570715
Imran Ahmed <imran.ahmed@ymail.com>
2024-02-16 22:34:30 +0000

    Node prompt sample

---
+++ index.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+
+const x = prompt('Enter x: ');
+const y = prompt('Enter y: ');
+const z = prompt('Enter z: ');
+
+console.log(parseInt(x) + parseInt(y) + parseInt(z));
